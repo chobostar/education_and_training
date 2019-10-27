914a82fc3eebd5ea20e3d0711d11864a140f88d8
chobostar <yakutskkirill@mail.ru>
2019-10-27 17:40:55 +0300

    add datastructures week4 BST practice

--- coursera/datastructures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ coursera/datastructures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
 
 import sys, threading
+from collections import deque
 
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
 def IsBinarySearchTree(tree):
   # Implement correct algorithm here
+  if len(tree) == 0:
+    return True
+  q = deque()
+  q.append((0,0))
+  while len(q) > 0:
+    node, parent = q.popleft()
+    if node == -1:
+      continue
+    if node != 0:
+      if tree[node][0] < tree[parent][0] and (
+              tree[node][1] != -1 and tree[tree[node][1]][0] > tree[parent][0]
+              or tree[node][2] != -1 and tree[tree[node][2]][0] >= tree[parent][0]):
+        return False
+      if tree[node][0] >= tree[parent][0] and (
+              tree[node][1] != -1 and tree[tree[node][1]][0] < tree[parent][0]
+              or tree[node][2] != -1 and tree[tree[node][2]][0] < tree[parent][0]):
+        return False
+    if tree[node][1] != -1 and tree[tree[node][1]][0] >= tree[node][0] \
+            or tree[node][2] != -1 and tree[tree[node][2]][0] < tree[node][0]:
+      return False
+    q.append((tree[node][1], node))
+    q.append((tree[node][2], node))
   return True
 
 
